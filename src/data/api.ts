86ffbe773bbeb318ecfeffe6e34148fa55ed4b70
TomekSetner <tomek.setner@gmail.com>
2023-06-10 20:09:53 +0200

    fix homepage

--- src/data/api.ts
+++ src/data/api.ts
@@ -2,8 +2,7 @@ import { Api } from "./generated-api";
 import { useStore } from "../domain/store";
 
 export const baseURL = "https://ticketex6.fly.dev";
-// export const baseURL = "http://localhost:8000";
-// export const baseURL = "https://ticketex2-production.up.railway.app";
+
 export const api = new Api({
   baseURL,
 });
